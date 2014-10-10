4f72b482b4db29094f19194f4448cd4ebc7630d3
nurzhan25 <nur92world@mail.ru>
2014-10-10 11:51:44 +0400

    Update libdb.c

--- libdb.c
+++ libdb.c
@@ -332,6 +332,7 @@ int b_tree_search(const struct DB *db, void *node, struct DBT *key, struct DBT *
 	block_ids = value_lens + key_number;
 	/* pointer to first key */
 	key_i_ptr = (void *)(block_ids + key_number + 1);
+	/* pointer to first value */
 	value_i_ptr = key_i_ptr;
 	for(i = 0; i < key_number; i++) {
 		value_i_ptr += key_lens[i];
@@ -340,26 +341,27 @@ int b_tree_search(const struct DB *db, void *node, struct DBT *key, struct DBT *
 	/* algorithm of b-tree-search from Corman*/
 	i = 0;
 	while( i < key_number 
-			&& (cmp = cmpkeys(key->data, key_i_ptr, key->size, key_lens[i])) > 0){
+			&& cmpkeys(key->data, key_i_ptr, key->size, key_lens[i]) > 0){
 		key_i_ptr += key_lens[i];
 		value_i_ptr += value_i_ptr[i];
 		i++;
 	}
 	
-	if (i < key_number && cmp == 0) {
+	if (i < key_number 
+			&& cmpkeys(key->data, key_i_ptr, key->size, key_lens[i]) == 0){
 		data->data = malloc(value_lens[i]);
-		memcpy(data->data, key_i_ptr, key_lens[i]);		
+		memcpy(data->data, value_i_ptr, value_lens[i]);
+		res = 0;
 	} else if (is_leaf) {
-		return -1;
+		res = -1;
 	} else {
 		child_node = malloc(db->db_info.chunk_size);
 		read_block_from_file(db, child_node, block_id[i]);
 		res = b_tree_search(db, child_node, key, data);
 		free(child_node);
-		return res;
 	}
 	
-	return 0;
+	return res;
 }
 
 int get(const struct DB *db, struct DBT *key, struct DBT *data)
