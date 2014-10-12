f10ca2ad949aa0443675b405f6034eefeec29aa4
nurzhan25 <nur92world@mail.ru>
2014-10-13 03:17:01 +0400

    Update libdb.c

--- libdb.c
+++ libdb.c
@@ -41,7 +41,7 @@ long int block_offset_in_file(const struct DB *db, int block_id)
 
 /* returns id of allocated block */
 /* numeration from 0 */
-int block_allocate(struct DB *db)
+int block_allocate(const struct DB *db)
 {
 	int i;
 	int j = 0;
@@ -368,8 +368,479 @@ int get(const struct DB *db, struct DBT *key, struct DBT *data)
 	 return b_tree_search(db, db->db_info.root_node, key, data);
 }
 
+int get_block_size(void *block)
+{
+	int block_size;
+	int key_count;
+	int *keys_size;
+	int *values_size;
+	int i;
+	
+	key_count = *(int *)(block);
+	keys_size = (int *)(block + sizeof(int) + sizeof(char));
+	values_size = keys_size + key_count;
+	
+	block_size = sizeof(char) + (2 + 3 * key_count) * sizeof(int);
+	
+	for(i = 0; i < key_count; i++)
+	{
+		block_size += keys_size[i];
+		block_size += values_size[i];
+	}
+	
+	return block_size;
+}
+
+/* m_leaf - modified leaf */
+void add_to_leaf(const struct DB *db, void *leaf, struct DBT *key,
+									struct DBT *data, void *m_leaf)
+{
+	int *leaf_key_count;
+	int *m_leaf_key_count;
+	/* */
+	int *leaf_keys_size;
+	int *m_leaf_keys_size;
+	/* */
+	int *leaf_values_size;
+	int *m_leaf_values_size;
+	/* */
+	void *leaf_keys;
+	void *m_leaf_keys;
+	/* */
+	void *leaf_values;
+	void *m_leaf_values;
+	
+	int i;
+	
+	leaf_key_count = (int *)leaf;
+	m_leaf_key_count = (int *)m_leaf;
+	/* key number and sign of leaf */
+	*m_leaf_key_count = *leaf_key_count + 1;
+	*(char *)(m_leaf + sizeof(int)) = (char) 1;
+	leaf_keys_size = (int *)(leaf + sizeof(int) + sizeof(char));
+	m_leaf_keys_size = (int *)(m_leaf + sizeof(int) + sizeof(char));
+	/* */
+	leaf_values_size = leaf_keys_size + *leaf_key_count;
+	m_leaf_values_size = m_leaf_keys_size + *m_leaf_key_count;	
+	/* setting leaf_keys and m_leaf_keys pointers */
+	leaf_keys = (leaf + sizeof(char) + (2 + 3 * (*leaf_key_count)) * sizeof(int));
+	m_leaf_keys = (m_leaf + sizeof(char) + (2 + 3 * (*m_leaf_key_count)) * sizeof(int));
+	
+	/* setting leaf_values and m_leaf_values pointers */
+	leaf_values = leaf_keys;
+	m_leaf_values = m_leaf_keys;
+	
+	for(i = 0; i < *leaf_key_count; i++)
+	{
+		leaf_values += leaf_keys_size[i];
+		m_leaf_values += leaf_keys_size[i];
+	}
+	
+	m_leaf_values += key->size;
+	
+	/* */
+	i = 0;
+	while(i < *leaf_key_count && cmpkeys(key->data, leaf_keys, key->size, leaf_keys_size[i]) > 0)
+	{
+		memcpy(m_leaf_keys, leaf_keys, leaf_keys_size[i]);
+		memcpy(m_leaf_values, leaf_values, leaf_values_size[i]);
+		m_leaf_keys_size[i] = leaf_keys_size[i];
+		m_leaf_values_size[i] = leaf_values_size[i];
+		leaf_keys += leaf_keys_size[i];
+		m_leaf_keys += leaf_keys_size[i];
+		leaf_values += leaf_values_size[i];
+		m_leaf_values += leaf_values_size[i];
+		i++;
+	}
+	
+	memcpy(m_leaf_keys, key->data, key->size);
+	memcpy(m_leaf_values, data->data, data->size);
+	m_leaf_keys += key->size;
+	m_leaf_values += data->size;
+	
+	while(i < *leaf_key_count)
+	{
+		memcpy(m_leaf_keys, leaf_keys, leaf_keys_size[i]);
+		memcpy(m_leaf_values, leaf_values, leaf_values_size[i]);
+		m_leaf_keys_size[i + 1] = leaf_keys_size[i];
+		m_leaf_values_size[i + 1] = leaf_values_size[i];
+		leaf_keys += leaf_keys_size[i];
+		m_leaf_keys += leaf_keys_size[i];
+		leaf_values += leaf_values_size[i];
+		m_leaf_values += leaf_values_size[i];
+		i++;
+	}
+
+	return;
+}
+
+int get_child_block_id_to_insert(const struct DB *db, void *node, struct DBT *key)
+{
+	int child_block_id;
+	int key_count;
+	int *keys_size;
+	int *blocks_id;
+	void *keys;
+	int i;
+	
+	key_count = *(int *)(node);
+	keys_size = (int *)(node + sizeof(int) + sizeof(char));
+	blocks_id = keys_size + 2 * key_count;
+	keys = ((void *)blocks_id) + key_count + 1;
+	
+	i = 0;
+	while( i < key_count && cmpkeys(key->data, keys, key->size, keys_size[i]) > 0)
+	{
+		keys += keys_size[i];
+		i++;
+	}
+
+	child_block_id = blocks_id[i];
+	
+	return child_block_id;
+}
+
+int get_split_point(const struct DB *db, void *node)
+{
+	int split_point;
+	int key_count;
+	int *keys_size;
+	int *values_size;
+	int left_size;
+	int i;
+	
+	key_count = *((int *)node);
+	keys_size = (int *)(node + sizeof(int) + sizeof(char));
+	values_size = keys_size + key_count;
+	
+	left_size = 2 * sizeof(int) + sizeof(char);
+	
+	i = 0;
+	while( left_size < (db->db_info.chunk_size / 2) )
+	{
+		left_size += 3 * sizeof(int);
+		left_size += keys_size[i];
+		left_size += values_size[i];
+		i++;
+	}
+	
+	split_point = i;
+	return split_point;
+}
+
+void split_child(const struct DB *db, void *parent, void *child, int child_block_id)
+{
+	/*t_parent - tmp parent*/
+	void *t_parent;
+	//int child_block_index;
+	int t_parent_size;
+	/* key count */
+	int *parent_key_count;
+	int *t_parent_key_count;
+	/* keys size */
+	int *parent_keys_size;
+	int *t_parent_keys_size;
+	/* values size */
+	int *parent_values_size;
+	int *t_parent_values_size;
+	/* blocks id */
+	int *parent_blocks_id;
+	int *t_parent_blocks_id;
+	/* keys */
+	void *parent_keys;
+	void *t_parent_keys;
+	/* values */
+	void *parent_values;
+	void *t_parent_values;
+	int i;
+	int split_point;
+	/* child - child1, child2 */
+	void *child1;
+	void *child2;
+	int child2_block_id;
+	/* key count */
+	int *child_key_count;
+	int *child1_key_count;
+	int *child2_key_count;
+	/* keys size */
+	int *child_keys_size;
+	int *child1_keys_size;
+	int *child2_keys_size;
+	/* values size */
+	int *child_values_size;
+	int *child1_values_size;
+	int *child2_values_size;
+	/* blocks id */
+	int *child_blocks_id;
+	int *child1_blocks_id;
+	int *child2_blocks_id;
+	/* keys */
+	void *child_keys;
+	void *child1_keys;
+	void *child2_keys;
+	/* values */
+	void *child_values;
+	void *child1_values;
+	void *child2_values;
+	/* */
+	void *keys_from;
+	void *values_from;
+	
+	
+	{
+		t_parent_size = 2 * db->db_info.chunk_size;
+		t_parent = malloc(t_parent_size);
+		parent_key_count = (int *) parent;
+		t_parent_key_count = (int *)t_parent;
+		*t_parent_key_count = *parent_key_count + 1;
+		*(char *)(t_parent + sizeof(int)) = (char) 1;
+		parent_keys_size = (int *)(parent + sizeof(int) + sizeof(char));
+		t_parent_keys_size = (int *)(t_parent + sizeof(int) + sizeof(char));
+		parent_values_size = parent_keys_size + *parent_key_count;
+		t_parent_values_size = t_parent_keys_size + *t_parent_key_count;
+		parent_blocks_id = parent_values_size + *parent_key_count;
+		t_parent_blocks_id = t_parent_values_size + *t_parent_key_count;
+		parent_keys = (void *)(parent_blocks_id + *parent_key_count + 1);
+		t_parent_keys = (void *)(t_parent_blocks_id + *t_parent_key_count + 1);
+	
+		parent_values = parent_keys;
+		t_parent_values = t_parent_keys;
+	
+		for(i = 0; i < *parent_key_count; i++) 
+		{
+			parent_values += parent_keys_size[i];
+			t_parent_values += parent_keys_size[i];
+		}
+	}
+	
+	
+	/* split child into child1 and child2 */
+	{
+		split_point = get_split_point(db, child);
+		child1 = malloc(db->db_info.chunk_size);
+		child2 = malloc(db->db_info.chunk_size);
+		
+		child_key_count = (int *) child;
+		child1_key_count = (int *) child1;
+		child2_key_count = (int *) child2;
+		
+		*child1_key_count = split_point;
+		*child2_key_count = *child_key_count - 1 - split_point;
+		
+		*(char *)(child1 + sizeof(int)) = *(char *)(child + sizeof(int));
+		*(char *)(child2 + sizeof(int)) = *(char *)(child + sizeof(int));
+		child_keys_size = (int *)(child + sizeof(int) + sizeof(char));
+		child1_keys_size = (int *)(child1 + sizeof(int) + sizeof(char));
+		child2_keys_size = (int *)(child2 + sizeof(int) + sizeof(char));
+		child_values_size = child_keys_size + *child_key_count;
+		child1_values_size = child1_keys_size + *child1_key_count;
+		child2_values_size = child1_keys_size + *child2_key_count;
+		child_blocks_id = child_values_size + *child_key_count;
+		child1_blocks_id = child1_values_size + *child1_key_count;
+		child2_blocks_id = child2_values_size + *child2_key_count;
+		/* keys and values */
+		child_keys = (void *)(child_blocks_id + *child_key_count + 1);
+		child1_keys = (void *)(child1_blocks_id + *child1_key_count + 1);
+		child2_keys = (void *)(child2_blocks_id + *child2_key_count + 1);
+		child_values = child_keys;
+		child1_values = child1_keys;
+		child2_values = child2_keys;
+		
+		for (i = 0; i < split_point; i++)
+		{
+			child1_keys_size[i] = child_keys_size[i];
+			child1_values_size[i] = child_values_size[i];
+			child1_blocks_id[i] = child_blocks_id[i];
+			child_values += child_keys_size[i];
+			child1_values += child1_keys_size[i];
+		}
+	
+		child1_blocks_id[split_point] = child_blocks_id[split_point];
+		child_values += child_keys_size[split_point];
+		
+		for(i = split_point + 1; i < *child_key_count; i++)
+		{
+			child2_keys_size[i - split_point - 1] = child_keys_size[i];
+			child2_values_size[i - split_point - 1] = child_values_size[i];
+			child2_blocks_id[i - split_point - 1] = child_blocks_id[i];
+			child_values += child_keys_size[i];
+			child2_values += child_keys_size[i];
+		}
+		
+		child2_blocks_id[*child_key_count - split_point - 1] = child_blocks_id[*child_key_count];
+		
+		for(i = 0; i < split_point; i++)
+		{
+			memcpy(child1_keys, child_keys, child_keys_size[i]);
+			memcpy(child1_values, child_values, child_values_size[i]);
+			child_keys += child_keys_size[i];
+			child1_keys_size += child_keys_size[i];
+			child_values += child_values_size[i];
+			child1_values += child_values_size[i];
+		}
+		
+		keys_from = child_keys;
+		values_from = child_values;
+		
+		child_keys += child_keys_size[split_point];
+		child_values += child_keys_size[split_point];
+		
+		for(i = split_point + 1; i < *child_key_count; i++)
+		{
+			memcpy(child2_keys, child_keys, child_keys_size[i]);
+			memcpy(child2_values, child_values, child_values_size[i]);
+			child_keys += child_keys_size[i];
+			child2_keys += child_keys_size[i];
+			child_values += child_values_size[i];
+			child2_values += child_values_size[i];
+		}
+	
+		child2_block_id = block_allocate(db);
+		write_block_to_file(db, child2, child_block_id);
+		write_block_to_file(db, child1, child2_block_id);
+		free(child1);
+		free(child2);	
+	}
+	
+	/* modify parent */
+	{
+		t_parent_values += child_keys_size[split_point];
+		//TODO
+		i = 0;
+		while( i < *parent_key_count
+					&& cmpkeys(keys_from, parent_keys, child_keys_size[split_point], parent_keys_size[i]) > 0)
+		{
+			memcpy(t_parent_keys, parent_keys, parent_keys_size[i]);
+			memcpy(t_parent_values, parent_values, parent_values_size[i]);
+			parent_keys += parent_keys_size[i];
+			t_parent_keys += parent_keys_size[i];
+			parent_values += parent_values_size[i];
+			t_parent_values += parent_values_size[i];
+			t_parent_keys_size[i] = parent_keys_size[i];
+			t_parent_values_size[i] = parent_values_size[i];
+			t_parent_blocks_id[i] = parent_blocks_id[i];
+			i++;
+		}
+		
+		memcpy(t_parent_keys, keys_from, child_keys_size[split_point]);
+		memcpy(t_parent_values, values_from, child_values_size[split_point]);
+		t_parent_keys_size[i] = child_keys_size[split_point];
+		t_parent_values_size[i] = child_values_size[split_point];
+		t_parent_blocks_id[i] = child_block_id;
+		printf("child_block_id = %d, parent_blocks_id[i] = %d\n", child_block_id, parent_blocks_id[i]);
+		t_parent_blocks_id[i + 1] = child2_block_id;
+		t_parent_keys += child_keys_size[split_point];
+		t_parent_values += child_values_size[split_point];
+		
+		while( i < *parent_key_count )
+		{
+			memcpy(t_parent_keys, parent_keys, parent_keys_size[i]);
+			memcpy(t_parent_values, parent_values, parent_values_size[i]);
+			parent_keys += parent_keys_size[i];
+			t_parent_keys += parent_keys_size[i];
+			parent_values += parent_values_size[i];
+			t_parent_values += parent_values_size[i];
+			t_parent_keys_size[i + 1] = parent_keys_size[i];
+			t_parent_values_size[i + 1] = parent_values_size[i];
+			t_parent_blocks_id[i + 2] = parent_blocks_id[i + 1];
+			i++;
+		}
+
+		memcpy(parent, t_parent, t_parent_size);
+	}
+	
+/* */
+	
+	free(t_parent);
+	return;
+}
 
 
+
+
+int b_tree_insert(const struct DB *db, void *node, struct DBT *key,
+									struct DBT *data, void *modified_parent, int block_id)
+{
+	/* TODO realize b_tree_insert */
+	
+	char is_leaf;
+	void *modified_me;
+	void *child_block;
+	int child_block_id;
+	int i_am_modified = 0;
+	int modified_me_size;	
+	
+	modified_me_size = 2 * db->db_info.chunk_size;
+	
+	is_leaf = *(char *)(node + sizeof(int));
+	modified_me = malloc(modified_me_size);
+	memcpy(modified_me, node, db->db_info.chunk_size);
+	
+	if(is_leaf) {
+		add_to_leaf(db, node, key, data, modified_me);
+		i_am_modified = 1;
+	} else {
+		child_block = malloc(db->db_info.chunk_size);
+		child_block_id = get_child_block_id_to_insert(db, node, key);
+		read_block_from_file(db, child_block, child_block_id);
+		i_am_modified = b_tree_insert(db, child_block, key, data, modified_me, child_block_id);
+		free(child_block);
+	}
+	
+	if(! i_am_modified){
+		free(modified_me);
+		return 0;
+	}
+	
+	
+	if( get_block_size(modified_me) <= db->db_info.chunk_size) {
+		memcpy(node, modified_me, db->db_info.chunk_size);
+		write_block_to_file(db, node, block_id);
+		free(modified_me);
+		return 0;
+	}
+	
+	//TODO split and etc.
+	split_child(db, modified_parent, modified_me, block_id);	
+	
+	free(modified_me);
+	return 1;
+}
+
+int put(const struct DB *db_in, struct DBT *key, struct DBT *data)
+{
+	void *pseudo_root;
+	int i_am_modified;
+	int pseudo_root_size;
+	int *key_count;
+	char *is_leaf;
+	int *child_id;
+	int new_block_id;
+	struct DB *db = db_in;
+	
+	pseudo_root_size = 2 * db->db_info.chunk_size;
+	pseudo_root = malloc(pseudo_root_size);
+	key_count = (int *)pseudo_root;
+	is_leaf = (char *)(pseudo_root + sizeof(int));
+	child_id = (int *)(pseudo_root + sizeof(int) + sizeof(char));
+	
+	*key_count = 0;
+	*is_leaf = 0;
+	*child_id = db->db_info.root_id;
+	
+	i_am_modified = b_tree_insert(db, db->db_info.root_node, key, data,
+										pseudo_root, db->db_info.root_id);
+							
+	if(i_am_modified) {
+		memcpy(db->db_info.root_node, pseudo_root, db->db_info.chunk_size);
+		new_block_id = block_allocate(db);
+		write_block_to_file(db, db->db_info.root_node, new_block_id);
+		db->db_info.root_id = new_block_id;
+	}
+	
+	free(pseudo_root);
+	return 0;
+}
  
 int main(int argc, char **argv) {
 #define N 15
