a678f307d550bfb195734d4f1c0df97c7379e21d
nurzhan25 <nur92world@mail.ru>
2014-10-22 03:17:04 +0400

    Update libdb.h

--- libdb.h
+++ libdb.h
@@ -18,7 +18,7 @@ struct DBC {
         size_t chunk_size; // block size
         /* Maximum memory size */
         /* 16MB by default */
-        size_t mem_size; 
+     //   size_t mem_size; 
 };
 
 typedef struct {
@@ -48,17 +48,76 @@ typedef struct {
 
 struct DB {
     /* Public API */
-    int (*close)(const struct DB *db);
-    int (*del)(const struct DB *db, const struct DBT *key);
-    int (*get)(const struct DB *db, struct DBT *key, struct DBT *data);
-    int (*put)(const struct DB *db, struct DBT *key, const struct DBT *data);
-    int (*sync)(const struct DB *db);
+    int (*close)(struct DB *db);
+    int (*del)(struct DB *db, struct DBT *key);
+    int (*get)(struct DB *db, struct DBT *key, struct DBT *data);
+    int (*put)(struct DB *db, struct DBT *key, struct DBT *data);
+    int (*sync)(struct DB *db);
+    /* Private API */
+	//db_info_in_DB db_info;
+    /* ... */
+}; /* Need for supporting multiple backends (HASH/BTREE) */
+
+struct MY_DB {
+	 /* Public API */
+    int (*close)(struct DB *db);
+    int (*del)(struct DB *db, struct DBT *key);
+    int (*get)(struct DB *db, struct DBT *key, struct DBT *data);
+    int (*put)(struct DB *db, struct DBT *key, struct DBT *data);
+    int (*sync)(struct DB *db);
     /* Private API */
 	db_info_in_DB db_info;
     /* ... */
 }; /* Need for supporting multiple backends (HASH/BTREE) */
 
 
-struct DB *dbcreate(const char *file, const struct DBC conf);
+struct DB *dbcreate(const char *file, struct DBC conf);
 struct DB *dbopen  (const char *file); /* Metadata in file */
+
+int close(struct DB *db);
+int del(struct DB *db, struct DBT *key);
+int get(struct DB *db, struct DBT *key, struct DBT *data);
+int put(struct DB *db, struct DBT *key, struct DBT *data);
+
+
+int db_close(struct DB *db)
+{
+	return close(db);
+}
+	
+int db_del(struct DB *db, void *k, size_t size)
+{
+	struct DBT key;
+	key.data = k;
+	key.size = size;
+	return del(db, &key);
+}
+
+int db_get(struct DB *db, void *k, size_t k_s, void **v, size_t *v_s)
+{
+	struct DBT key;
+	struct DBT value;
+	int res;
+	key.data = k;
+	key.size = k_s;
+	res = get(db, &key, &value);
+	*v = value.data;
+	*v_s = value.size;
+	return  res;
+}	
+	
+int db_put(struct DB *db, void *k, size_t k_s, void * v, size_t v_s )
+{
+	struct DBT key;
+	struct DBT value;
+	int res;
+	key.data = k;
+	key.size = k_s;
+	value.data = v;
+	value.size = v_s;
+	return put(db, &key, &value);
+}
+
+
+
 
